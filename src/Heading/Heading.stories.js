1d6c72aca63a604bcf9ded21a603a44831057600
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-05 17:28:31 -0300

    add Heading stories

---
+++ src/Heading/Heading.stories.js
@@ -0,0 +1,39 @@
+import { Heading } from '.';
+
+export default {
+  title: 'Heading',
+  component: Heading,
+  args: {
+    children: 'Some text',
+    gradient: null,
+    as: 'h1',
+  },
+  argTypes: {
+    children: { type: 'string' },
+  },
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+    },
+  },
+};
+
+export const Default = (args) => <Heading {...args} />;
+
+export const WithBlueGradient = Default.bind({});
+
+WithBlueGradient.args = {
+  gradient: 'blue',
+};
+
+export const WithGreenGradient = Default.bind({});
+
+WithGreenGradient.args = {
+  gradient: 'green',
+};
+
+export const AsHeading5 = Default.bind({});
+
+AsHeading5.args = {
+  as: 'h5',
+};
