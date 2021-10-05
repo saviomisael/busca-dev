1f396986fc066a22ee57191264bd4bec22997448
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-05 15:59:29 -0300

    add theme file

---
+++ src/styles/theme.js
@@ -0,0 +1,22 @@
+export const theme = {
+  colors: {
+    first: '#284B63',
+    second: '#3C6E71',
+    white: '#F5F5F5',
+    gray: '#D9D9D9',
+    black: '#353535',
+  },
+  fonts: {
+    family: {
+      notoSans: `'Noto Sans', sans-serif`,
+      ubuntu: `'Ubuntu', sans-serif`,
+    },
+    sizes: {
+      default: '1.6rem',
+      small: '1.4rem',
+      medium: '1.8rem',
+      large: '2.2rem',
+      huge: '4rem',
+    },
+  },
+};
