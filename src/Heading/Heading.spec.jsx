4cd0342a415e2c55e19c0b19bc5629c453a7da5b
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-05 17:50:25 -0300

    pass test 'should be a h5 element when as prop is h5'

--- src/Heading/Heading.spec.jsx
+++ src/Heading/Heading.spec.jsx
@@ -11,4 +11,12 @@ describe('<Heading />', () => {
 
     expect(heading).not.toHaveStyle('color: transparent');
   });
+
+  it('should be a h5 element when as prop is h5', () => {
+    global.renderTheme(<Heading as="h5">Children</Heading>);
+
+    const heading = screen.getByText('Children');
+
+    expect(heading.tagName.toLowerCase()).toBe('h5');
+  });
 });
