431d519aa8567a41a6689883cdf4c524cd662dcc
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-05 17:27:47 -0300

    add Heading component styles

---
+++ src/Heading/styles.js
@@ -0,0 +1,28 @@
+import styled, { css } from 'styled-components';
+
+const gradientBackground = css`
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
+`;
+
+const gradientsTitle = {
+  blue: (theme) => css`
+    background: linear-gradient(${theme.colors.white}, ${theme.colors.first});
+    ${gradientBackground}
+  `,
+  green: (theme) => css`
+    background: linear-gradient(${theme.colors.white}, ${theme.colors.second});
+    ${gradientBackground}
+  `,
+};
+
+export const Title = styled.h1`
+  ${({ theme, gradient }) => css`
+    color: ${theme.colors.white};
+    font-weight: 400;
+    font-family: ${theme.fonts.family.ubuntu};
+
+    ${gradient && gradientsTitle[gradient](theme)}
+  `}
+`;
