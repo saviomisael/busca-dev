e8c0123c79515f0de02721d649681875af82ec3f
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-06 14:00:43 -0300

    add ProfileBox props mock

---
+++ src/components/ProfileBox/mock.js
@@ -0,0 +1,9 @@
+import Image from '../../assets/images/person_white.svg';
+
+export const profileBoxPropsMock = {
+  avatar: Image,
+  username: 'johndoe',
+  fullname: 'John Doe',
+  bio: 'bio Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  blog: 'blog.com',
+};
