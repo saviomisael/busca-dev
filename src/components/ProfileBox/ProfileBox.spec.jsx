b994e1beeacf80076d768038e35354a41d6e169e
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-06 14:12:16 -0300

    pass test 'should renders component with default props'

---
+++ src/components/ProfileBox/ProfileBox.spec.jsx
@@ -0,0 +1,22 @@
+import { screen } from '@testing-library/react';
+import { ProfileBox } from '.';
+import { profileBoxPropsMock } from './mock';
+
+describe('<ProfileBox />', () => {
+  it('should renders component with default props', () => {
+    delete profileBoxPropsMock.bio;
+    delete profileBoxPropsMock.blog;
+
+    global.renderTheme(<ProfileBox {...profileBoxPropsMock} />);
+
+    const bioElement = screen.queryByText(
+      'bio Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    );
+
+    const blogElement = screen.queryByRole('link', { name: 'blog.com' });
+
+    expect(bioElement).not.toBeInTheDocument();
+
+    expect(blogElement).not.toBeInTheDocument();
+  });
+});
