e0d329b270e7c439863fd2c327ad6c5c0ec85f0b
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-06 16:59:37 -0300

    add Button component stories

---
+++ src/components/Button/Button.stories.js
@@ -0,0 +1,33 @@
+import { Button } from '.';
+
+export default {
+  title: 'Button',
+  component: Button,
+  args: {
+    children: 'Some text',
+    size: 'large',
+  },
+  argTypes: {
+    children: { type: 'string' },
+    onClick: { table: { disable: true } },
+  },
+  parameters: {
+    backgrounds: {
+      default: 'light',
+    },
+  },
+};
+
+export const Default = (args) => <Button {...args} />;
+
+export const Hover = Default.bind({});
+
+Hover.parameters = {
+  pseudo: { hover: true },
+};
+
+export const WhenClicked = Default.bind({});
+
+WhenClicked.parameters = {
+  pseudo: { active: true },
+};
