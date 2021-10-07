8de18f543465c0798d9e3b9b6a63335282867bd5
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-07 16:22:55 -0300

    add RepoCard props mock

---
+++ src/components/RepoCard/mock.js
@@ -0,0 +1,6 @@
+export const repoCardPropsMock = {
+  repoName: 'repo name',
+  description: 'Repository Description lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  starsCount: 0,
+  repoUrl: 'github.com/repo',
+};
