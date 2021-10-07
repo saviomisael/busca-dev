5d2e471d75ecb3c0ccf87e7955f3c21d02468c07
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-07 16:02:35 -0300

    refactor StarsCounter component

--- src/components/RepoCard/StarsCounter/index.jsx
+++ src/components/RepoCard/StarsCounter/index.jsx
@@ -1,10 +1,18 @@
 import PropTypes from 'prop-types';
 import * as Styled from './styles';
+import StarEmpty from '../../../assets/images/star_empty.svg';
+import StarFilled from '../../../assets/images/star_fill.svg';
 
 export const StarsCounter = ({ starsCount }) => {
   const hasStars = starsCount > 0;
+  const icon = hasStars ? StarFilled : StarEmpty;
 
-  return <Styled.StarsCounter hasStars={hasStars}>{starsCount} Estrelas</Styled.StarsCounter>;
+  return (
+    <Styled.Wrapper>
+      <Styled.Icon src={icon} alt="star icon" />
+      <Styled.StarsCounter hasStars={hasStars}>{starsCount} Estrelas</Styled.StarsCounter>
+    </Styled.Wrapper>
+  );
 };
 
 StarsCounter.propTypes = {
--- src/components/RepoCard/StarsCounter/styles.js
+++ src/components/RepoCard/StarsCounter/styles.js
@@ -1,23 +1,20 @@
 import styled, { css } from 'styled-components';
-import StarEmpty from '../../../assets/images/star_empty.svg';
-import StarFilled from '../../../assets/images/star_fill.svg';
 
-export const StarsCounter = styled.p`
-  text-transform: uppercase;
+export const Wrapper = styled.div`
   display: flex;
   align-items: center;
   column-gap: 0.7rem;
+`;
+
+export const StarsCounter = styled.p`
+  text-transform: uppercase;
 
   ${({ theme }) => css`
     font-size: ${theme.fonts.sizes.default};
     color: ${theme.colors.first};
   `}
+`;
 
-  &::before {
-    filter: invert(23%) sepia(6%) saturate(4839%) hue-rotate(162deg) brightness(93%) contrast(82%);
-
-    ${({ hasStars }) => css`
-      content: url(${hasStars ? StarFilled : StarEmpty});
-    `}
-  }
+export const Icon = styled.img`
+  filter: invert(23%) sepia(6%) saturate(4839%) hue-rotate(162deg) brightness(93%) contrast(82%);
 `;
