7fc7bf3170a2494c698f30a32fc8b7c03e0e21f7
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-07 10:01:02 -0300

    refactor Button test

--- src/components/Button/Button.spec.jsx
+++ src/components/Button/Button.spec.jsx
@@ -2,9 +2,9 @@ import { screen } from '@testing-library/react';
 import { Button } from '.';
 
 describe('<Button />', () => {
-  it('should renders component with default props', () => {
-    const onClickMock = jest.fn();
+  const onClickMock = jest.fn();
 
+  it('should renders component with default props', () => {
     global.renderTheme(<Button onClick={onClickMock}>Button</Button>);
 
     const button = screen.getByRole('button', { name: 'Button' });
@@ -13,8 +13,6 @@ describe('<Button />', () => {
   });
 
   it('should renders component with medium size', () => {
-    const onClickMock = jest.fn();
-
     global.renderTheme(
       <Button onClick={onClickMock} size="medium">
         Button
