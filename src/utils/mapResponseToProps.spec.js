f05a93c78d07403d112f43d6ebe920e18789afc4
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-08 16:09:41 -0300

    pass test 'should map repositories response to props'

--- src/utils/mapResponseToProps.spec.js
+++ src/utils/mapResponseToProps.spec.js
@@ -1,4 +1,4 @@
-import { mapUserResponseToProps } from './mapResponseToProps';
+import { mapUserResponseToProps, mapReposResponseOrderByStarsCount } from './mapResponseToProps';
 
 describe('mapResponseToProps', () => {
   const responseMock = {
@@ -18,5 +18,23 @@ describe('mapResponseToProps', () => {
     expect(result.fullname).toEqual(responseMock.name);
     expect(result.bio).toEqual(responseMock.bio);
     expect(result.blog).toEqual(responseMock.blog);
+  });
+});
+
+describe('mapReposResponseOrderByStarsCount', () => {
+  const reposResponseMock = [
+    { id: 1, name: 'repo 1', description: 'description 1', stargazers_count: 0, url: 'repo1.com' },
+    { id: 2, name: 'repo 2', description: 'description 2', stargazers_count: 3, url: 'repo2.com' },
+    { id: 3, name: 'repo 3', description: 'description 3', stargazers_count: 2, url: 'repo3.com' },
+  ];
+
+  it('should map repositories response to props', () => {
+    const result = mapReposResponseOrderByStarsCount([reposResponseMock[0]]);
+
+    expect(result[0].repoId).toBe(1);
+    expect(result[0].repoName).toEqual('repo 1');
+    expect(result[0].description).toEqual('description 1');
+    expect(result[0].starsCount).toEqual(0);
+    expect(result[0].repoUrl).toEqual('repo1.com');
   });
 });
