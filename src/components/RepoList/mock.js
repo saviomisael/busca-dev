9537ea98b0b45a284079b1ebfd5e873659bf251d
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-08 11:07:42 -0300

    add RepoList props mock

---
+++ src/components/RepoList/mock.js
@@ -0,0 +1,11 @@
+export const repoListPropsMock = {
+  repositories: [
+    {
+      repoId: 1,
+      repoName: 'repo name',
+      description: 'Repository Description lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      starsCount: 0,
+      repoUrl: 'github.com/repo',
+    },
+  ],
+};
