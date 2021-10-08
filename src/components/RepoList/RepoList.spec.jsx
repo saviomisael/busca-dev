bf2dbbbb1234ac89b09d5d44185e91f3b44e2d85
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-08 11:29:01 -0300

    pass test 'should renders a list of RepoCard when repositories length greater than zero'

--- src/components/RepoList/RepoList.spec.jsx
+++ src/components/RepoList/RepoList.spec.jsx
@@ -1,5 +1,6 @@
 import { screen } from '@testing-library/react';
 import { RepoList } from '.';
+import { repoListPropsMock } from './mock';
 
 describe('<RepoList />', () => {
   it('should renders list empty message when repositories list is empty', () => {
@@ -8,5 +9,13 @@ describe('<RepoList />', () => {
     const message = screen.getByText('Este usuário não possui repositórios');
 
     expect(message).toBeInTheDocument();
+  });
+
+  it('should renders a list of RepoCard when repositories length greater than zero', () => {
+    global.renderTheme(<RepoList {...repoListPropsMock} />);
+
+    const listItem = screen.getAllByRole('listitem');
+
+    expect(listItem.length).toBe(1);
   });
 });
