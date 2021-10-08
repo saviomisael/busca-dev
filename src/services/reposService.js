b2cd37b1db9408d60f25d9379273d53fbd21d860
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-08 15:57:09 -0300

    add reposService

---
+++ src/services/reposService.js
@@ -0,0 +1,8 @@
+import api from './api';
+import { mapReposResponseOrderByStarsCount } from '../utils/mapResponseToProps';
+
+export const getReposByUsernameOrderByStarsCount = async (username) => {
+  const response = await api.get(`/users/${username}/repos`);
+
+  return mapReposResponseOrderByStarsCount(response);
+};
--- src/utils/mapResponseToProps.js
+++ src/utils/mapResponseToProps.js
@@ -3,3 +3,17 @@ export const mapUserResponseToProps = (response) => {
 
   return { avatar: avatar_url, username: login, fullname: name, bio, blog };
 };
+
+const mapSingleRepoResponseToProps = (singleRepo) => {
+  const { id, name, description, stargazers_count, url } = singleRepo;
+
+  return { repoId: id, repoName: name, description, starsCount: stargazers_count, repoUrl: url };
+};
+
+export const mapReposResponseOrderByStarsCount = (response) => {
+  return response
+    .map((x) => mapSingleRepoResponseToProps(x))
+    .sort((a, b) => {
+      return a.starsCount - b.starsCount;
+    });
+};
