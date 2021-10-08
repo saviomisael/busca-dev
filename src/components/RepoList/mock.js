38bcc8d44b2038e2997698f840472ff59156dfa5
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-08 11:40:47 -0300

    add many repositories mock

--- src/components/RepoList/mock.js
+++ src/components/RepoList/mock.js
@@ -9,3 +9,36 @@ export const repoListPropsMock = {
     },
   ],
 };
+
+export const manyRepositories = {
+  repositories: [
+    {
+      repoId: 1,
+      repoName: 'repo name',
+      description: 'Repository Description lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      starsCount: 0,
+      repoUrl: 'github.com/repo',
+    },
+    {
+      repoId: 2,
+      repoName: 'repo name',
+      description: 'Repository Description lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      starsCount: 3,
+      repoUrl: 'github.com/repo',
+    },
+    {
+      repoId: 3,
+      repoName: 'repo name',
+      description: 'Repository Description lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      starsCount: 0,
+      repoUrl: 'github.com/repo',
+    },
+    {
+      repoId: 4,
+      repoName: 'repo name',
+      description: 'Repository Description lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      starsCount: 5,
+      repoUrl: 'github.com/repo',
+    },
+  ],
+};
