eb8841613054e62d4f053b428c9889a76d664864
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-08 11:07:18 -0300

    add RepoList component

---
+++ src/components/RepoList/index.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import * as Styled from './styles';
+import { RepoCard } from '../RepoCard';
+
+export const RepoList = ({ repositories }) => {
+  let content = repositories.length === 0 && (
+    <Styled.ListEmptyMessage>Este usuário não possui repositórios</Styled.ListEmptyMessage>
+  );
+
+  if (repositories.length > 0) {
+    content = repositories.map(({ repoId, ...rest }) => (
+      <Styled.Item key={repoId}>
+        <RepoCard {...rest} />
+      </Styled.Item>
+    ));
+  }
+
+  return <Styled.List>{content}</Styled.List>;
+};
+
+RepoList.propTypes = {
+  repositories: PropTypes.arrayOf(
+    PropTypes.shape({
+      repoId: PropTypes.number.isRequired,
+      repoName: PropTypes.string.isRequired,
+      description: PropTypes.string,
+      starsCount: PropTypes.number.isRequired,
+      repoUrl: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
