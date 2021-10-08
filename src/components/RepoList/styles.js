01d47bf073e444759be80a4d2eab8981d497f8ae
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-08 11:41:18 -0300

    refactor RepoList styles

--- src/components/RepoList/styles.js
+++ src/components/RepoList/styles.js
@@ -2,8 +2,11 @@ import styled, { css } from 'styled-components';
 import * as TextComponentStyled from '../TextComponent/styles';
 
 export const List = styled.ul`
-  padding: 1rem;
+  padding: 2rem;
   border-radius: 0.6rem;
+  display: flex;
+  flex-direction: column;
+  row-gap: 2rem;
 
   ${({ theme }) => css`
     background: linear-gradient(${theme.colors.gray + 'BB'}, ${theme.colors.black + 'CC'});
@@ -17,8 +20,10 @@ export const ListEmptyMessage = styled(TextComponentStyled.Text)`
   text-align: center;
   text-transform: uppercase;
   font-weight: 600;
+  border-radius: 0.6rem;
 
   ${({ theme }) => css`
     color: ${theme.colors.black};
+    background: ${theme.colors.gray};
   `}
 `;
