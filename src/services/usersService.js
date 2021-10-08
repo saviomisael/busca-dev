7a120eac73f8659ecb4f106eddcf133f8f16bb76
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-08 17:03:56 -0300

    refactor users service

--- src/services/usersService.js
+++ src/services/usersService.js
@@ -1,8 +1,17 @@
+import axios from 'axios';
 import api from './api';
 import { mapUserResponseToProps } from '../utils/mapResponseToProps';
 
+const cancelRequest = axios.CancelToken.source();
+
 export const getUserInfo = async (username) => {
-  const response = await api.get(`/users/${username}`);
+  const response = await api.get(`/users/${username}`, {
+    cancelToken: cancelRequest.token,
+  });
 
   return mapUserResponseToProps(response);
+};
+
+export const cancelGetUserInfo = () => {
+  cancelRequest.cancel();
 };
