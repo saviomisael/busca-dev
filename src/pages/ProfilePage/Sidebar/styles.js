e005584b69900cc9f5d310f0ffefa66fc9adef28
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-11 12:22:46 -0300

    add Sidebar styles

---
+++ src/pages/ProfilePage/Sidebar/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { Button } from '../../../components/Button/styles';
+
+export const Wrapper = styled.aside`
+  > ${Button} {
+    margin-top: 0.8rem;
+  }
+`;
