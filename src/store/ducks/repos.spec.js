5c88e147ebf70e459b53887da071fea50598776c
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-11 10:53:10 -0300

    pass test 'should insert repositories when dispatch inserRepositories'

--- src/store/ducks/repos.spec.js
+++ src/store/ducks/repos.spec.js
@@ -1,4 +1,4 @@
-import { reposReducer } from './repos';
+import { reposActions, reposReducer } from './repos';
 
 describe('repos duck', () => {
   it('should return initial state', () => {
@@ -6,6 +6,22 @@ describe('repos duck', () => {
 
     expect(reposState).toEqual({
       repositories: [],
+      hasError: false,
+    });
+  });
+
+  it('should insert repositories when dispatch inserRepositories', () => {
+    const initialState = {
+      repositories: [],
+      hasError: false,
+    };
+
+    const payload = [{ id: 1, name: 'repo 1', description: 'description 1', stargazers_count: 0, url: 'repo1.com' }];
+
+    const reposState = reposReducer(initialState, reposActions.insertRepositories(payload));
+
+    expect(reposState).toEqual({
+      repositories: [{ id: 1, name: 'repo 1', description: 'description 1', stargazers_count: 0, url: 'repo1.com' }],
       hasError: false,
     });
   });
