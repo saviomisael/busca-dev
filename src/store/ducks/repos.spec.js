17767283dcb272e3ea10102976b236ce80d548f6
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-11 10:54:25 -0300

    refactor repos duck test

--- src/store/ducks/repos.spec.js
+++ src/store/ducks/repos.spec.js
@@ -1,21 +1,18 @@
 import { reposActions, reposReducer } from './repos';
 
 describe('repos duck', () => {
+  const initialState = {
+    repositories: [],
+    hasError: false,
+  };
+
   it('should return initial state', () => {
     const reposState = reposReducer(undefined, {});
 
-    expect(reposState).toEqual({
-      repositories: [],
-      hasError: false,
-    });
+    expect(reposState).toEqual(initialState);
   });
 
   it('should insert repositories when dispatch inserRepositories', () => {
-    const initialState = {
-      repositories: [],
-      hasError: false,
-    };
-
     const payload = [{ id: 1, name: 'repo 1', description: 'description 1', stargazers_count: 0, url: 'repo1.com' }];
 
     const reposState = reposReducer(initialState, reposActions.insertRepositories(payload));
