65eab4e9d63d19be457416110f9c2910e4c579ce
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-11 10:31:57 -0300

    refactor users duck test

--- src/store/ducks/users.spec.js
+++ src/store/ducks/users.spec.js
@@ -1,31 +1,22 @@
 import { usersActions, usersReducer } from './users';
 
 describe('users duck', () => {
+  const initialState = {
+    userInfo: {
+      avatar: '',
+      username: '',
+      fullname: '',
+      bio: '',
+      blog: '',
+    },
+    userNotFoundStatus: false,
+  };
+
   it('should return the initial state', () => {
-    expect(usersReducer(undefined, {})).toEqual({
-      userInfo: {
-        avatar: '',
-        username: '',
-        fullname: '',
-        bio: '',
-        blog: '',
-      },
-      userNotFoundStatus: false,
-    });
+    expect(usersReducer(undefined, {})).toEqual(initialState);
   });
 
   it('should change user info', () => {
-    const previousState = {
-      userInfo: {
-        avatar: '',
-        username: '',
-        fullname: '',
-        bio: '',
-        blog: '',
-      },
-      userNotFoundStatus: false,
-    };
-
     const payload = {
       avatar: 'avatar.png',
       username: 'johndoe',
@@ -34,7 +25,7 @@ describe('users duck', () => {
       blog: 'blog.com',
     };
 
-    expect(usersReducer(previousState, usersActions.changeUserInfo(payload))).toEqual({
+    expect(usersReducer(initialState, usersActions.changeUserInfo(payload))).toEqual({
       userInfo: {
         avatar: 'avatar.png',
         username: 'johndoe',
