9be6753aad2579a5c7caac2d09e78ccb59489308
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-11 12:31:18 -0300

    add renderStoreProviderWithTheme function

--- src/setupTests.js
+++ src/setupTests.js
@@ -7,7 +7,16 @@ import 'jest-styled-components';
 import { render } from '@testing-library/react';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
+import { Provider } from 'react-redux';
+import { storeMock } from './store/store-mock';
 
 global.renderTheme = (children) => {
   return render(<ThemeProvider theme={theme}>{children}</ThemeProvider>);
 };
+
+global.renderStoreProviderWithTheme = (children) =>
+  render(
+    <Provider store={storeMock}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </Provider>,
+  );
