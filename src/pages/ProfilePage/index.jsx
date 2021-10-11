ab6bf0347fcb10578d0023585ee828f78170e8b5
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-11 18:21:46 -0300

    refactor ProfilePage component

--- src/pages/ProfilePage/index.jsx
+++ src/pages/ProfilePage/index.jsx
@@ -1,9 +1,29 @@
+import { useParams } from 'react-router-dom';
 import * as Styled from './styles';
 import { Sidebar } from './Sidebar';
 import { Heading } from '../../components/Heading';
 import { MainContent } from './MainContent';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getUserInfoThunk } from '../../store/ducks/users';
+import { getRepositoriesThunk } from '../../store/ducks/repos';
+import { cancelGetUserInfo } from '../../services/usersService';
+import { cancelGetRepos } from '../../services/reposService';
 
 export const ProfilePage = () => {
+  const { username } = useParams();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUserInfoThunk(username));
+    dispatch(getRepositoriesThunk(username));
+
+    return () => {
+      cancelGetUserInfo();
+      cancelGetRepos();
+    };
+  }, [username, dispatch]);
+
   return (
     <Styled.Container>
       <Heading gradient="blue">Perfil</Heading>
