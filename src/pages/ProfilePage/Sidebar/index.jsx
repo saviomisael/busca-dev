acfb1768dfff0633adde64a260709fa237a12228
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-11 12:23:10 -0300

    add Sidebar component

---
+++ src/pages/ProfilePage/Sidebar/index.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import * as Styled from './styles';
+import { ProfileBox } from '../../../components/ProfileBox';
+import { Button } from '../../../components/Button';
+import { useSelector } from 'react-redux';
+
+export const Sidebar = () => {
+  const { userInfo } = useSelector((state) => state.users);
+
+  const handleButtonClick = () => {};
+
+  return (
+    <Styled.Wrapper>
+      <ProfileBox {...userInfo} />
+      <Button onClick={handleButtonClick}>Voltar</Button>
+    </Styled.Wrapper>
+  );
+};
+
+Sidebar.propTypes = {
+  children: PropTypes.node.isRequired,
+};
