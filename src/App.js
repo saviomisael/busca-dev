e600e95d6ae6c1f68309022a8b9afff067741707
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-11 11:36:06 -0300

    add StoreProvider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { ThemeProvider } from 'styled-components';
+import { StoreProvider } from './store/StoreProvider';
 import { BaseStyles } from './styles/base';
 import { theme } from './styles/theme';
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <BaseStyles />
-      <h1>Oi</h1>
-    </ThemeProvider>
+    <StoreProvider>
+      <ThemeProvider theme={theme}>
+        <BaseStyles />
+        <h1>Oi</h1>
+      </ThemeProvider>
+    </StoreProvider>
   );
 };
 
