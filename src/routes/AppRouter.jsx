678a5bf8c64265bd41e432eb15404f5825e2d9ed
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-15 20:24:50 -0300

    add Home Route

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { HomePage } from '../pages/HomePage';
 import { ProfilePage } from '../pages/ProfilePage';
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/" exact component={HomePage} />
         <Route path="/profile/:username" component={ProfilePage} />
       </Switch>
     </BrowserRouter>
