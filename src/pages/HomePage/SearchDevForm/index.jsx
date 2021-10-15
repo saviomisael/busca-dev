c0e60796a8a99cf397b74e0b0cc84728b2a8f6de
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-15 19:37:41 -0300

    add SearchDevForm component

---
+++ src/pages/HomePage/SearchDevForm/index.jsx
@@ -0,0 +1,25 @@
+import { useForm } from 'react-hook-form';
+import PropTypes from 'prop-types';
+import * as Styled from './styles';
+import { SearchIcon } from '../../../components/SearchIcon';
+
+export const SearchDevForm = ({ onSubmit }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitted },
+  } = useForm();
+
+  return (
+    <Styled.Form onSubmit={handleSubmit(onSubmit)}>
+      <Styled.Input placeholder="Insira aqui o username do github" {...register('username')} />
+      <Styled.ButtonSubmit disabled={isSubmitted}>
+        <SearchIcon />
+      </Styled.ButtonSubmit>
+    </Styled.Form>
+  );
+};
+
+SearchDevForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
