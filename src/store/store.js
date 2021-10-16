7122597c94d27733853c475b201c4c285b49e363
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-16 15:49:34 -0300

    add ui duck

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { reposReducer } from './ducks/repos';
 import { usersReducer } from './ducks/users';
+import { uiReducer } from './ducks/ui';
 
 export const store = configureStore({
-  reducer: { users: usersReducer, repos: reposReducer },
+  reducer: { users: usersReducer, repos: reposReducer, ui: uiReducer },
 });
