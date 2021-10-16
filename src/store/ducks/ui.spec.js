0543792e0bad23e1f68440b706a310869dfb98dc
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-16 15:52:00 -0300

    refactor ui duck test

--- src/store/ducks/ui.spec.js
+++ src/store/ducks/ui.spec.js
@@ -1,21 +1,17 @@
 import { uiActions, uiReducer } from './ui';
 
 describe('ui duck', () => {
-  it('should set isLoading to true', () => {
-    const initialState = {
-      isLoading: false,
-    };
+  const initialState = {
+    isLoading: false,
+  };
 
+  it('should set isLoading to true', () => {
     const modifiedState = uiReducer(initialState, uiActions.setIsLoading(true));
 
     expect(modifiedState.isLoading).toBe(true);
   });
 
   it('should set isLoading to false', () => {
-    const initialState = {
-      isLoading: false,
-    };
-
     const modifiedState = uiReducer(initialState, uiActions.setIsLoading(false));
 
     expect(modifiedState.isLoading).toBe(false);
