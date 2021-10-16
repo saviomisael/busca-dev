4fce1df807000ff50b8f15405e23ae0963918789
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-16 14:32:26 -0300

    remove code

--- src/services/reposService.js
+++ src/services/reposService.js
@@ -1,17 +1,8 @@
-import axios from 'axios';
 import api from './api';
 import { mapReposResponseOrderByStarsCount } from '../utils/mapResponseToProps';
 
-const cancelRequest = axios.CancelToken.source();
-
 export const getReposByUsernameOrderByStarsCount = async (username) => {
-  const response = await api.get(`/users/${username}/repos`, {
-    cancelToken: cancelRequest.token,
-  });
+  const response = await api.get(`/users/${username}/repos`, {});
 
   return mapReposResponseOrderByStarsCount(response.data);
-};
-
-export const cancelGetRepos = () => {
-  cancelRequest.cancel();
 };
