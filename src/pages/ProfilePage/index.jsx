469464457fb6d233752911ed4eac32a1c6cfd98d
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-16 17:40:30 -0300

    refactor ProfilePage

--- src/pages/ProfilePage/index.jsx
+++ src/pages/ProfilePage/index.jsx
@@ -1,28 +1,43 @@
 import { useParams } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
+import { Helmet } from 'react-helmet-async';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import * as Styled from './styles';
 import { Sidebar } from './Sidebar';
 import { Heading } from '../../components/Heading';
 import { MainContent } from './MainContent';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { LoadingMessage } from '../../components/LoadingMessage';
 import { getUserInfoThunk } from '../../store/ducks/users';
 import { getRepositoriesThunk } from '../../store/ducks/repos';
 
 export const ProfilePage = () => {
   const { username } = useParams();
   const dispatch = useDispatch();
+  const { isLoading } = useSelector((state) => state.ui);
+  let title = (
+    <Helmet titleTemplate="%s | Busca Dev">
+      <title>Perfil</title>
+    </Helmet>
+  );
 
   useEffect(() => {
     dispatch(getUserInfoThunk(username));
     dispatch(getRepositoriesThunk(username));
   }, [username, dispatch]);
 
+  if (isLoading) {
+    return (
+      <Styled.Container>
+        {title}
+        <LoadingMessage />
+      </Styled.Container>
+    );
+  }
+
   return (
     <Styled.Container>
-      <Helmet titleTemplate="%s | Busca Dev">
-        <title>Perfil</title>
-      </Helmet>
+      {title}
       <Heading gradient="blue">Perfil</Heading>
       <Styled.Content>
         <Sidebar />
