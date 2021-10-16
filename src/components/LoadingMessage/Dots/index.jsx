15a98473f5e097fa81640dd07770e43baafde9ba
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-16 16:59:08 -0300

    add Dots component

---
+++ src/components/LoadingMessage/Dots/index.jsx
@@ -0,0 +1,11 @@
+import * as Styled from './styles';
+
+export const Dots = () => {
+  return (
+    <>
+      <Styled.Dot>&middot;</Styled.Dot>
+      <Styled.Dot>&middot;</Styled.Dot>
+      <Styled.Dot>&middot;</Styled.Dot>
+    </>
+  );
+};
