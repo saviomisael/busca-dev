9d64793b3bd3eb2f1350af4a1ebdf316021ef929
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-10-21 13:30:40 -0300

    refactor homepage_spec

--- cypress/integration/homepage_spec.js
+++ cypress/integration/homepage_spec.js
@@ -1,10 +1,7 @@
 /// <reference types="Cypress" />
 describe('home page', () => {
   it('should submit form with a username valid then redirect to profile page', () => {
-    cy.visit('/');
-
-    cy.get('[data-cy=username-input]').type('saviomisael');
-    cy.get('[data-cy=submit-search-dev]').click();
+    cy.submitSearchDevForm('saviomisael');
 
     cy.location().should((loc) => {
       expect(loc.pathname).contains('profile/saviomisael');
@@ -12,10 +9,7 @@ describe('home page', () => {
   });
 
   it('should redirect to not found page when user not exist', () => {
-    cy.visit('/');
-
-    cy.get('[data-cy=username-input]').type('chapolinwayne');
-    cy.get('[data-cy=submit-search-dev]').click();
+    cy.submitSearchDevForm('chapolinwayne');
 
     cy.location().should((loc) => {
       expect(loc.pathname).contains('notfound');
